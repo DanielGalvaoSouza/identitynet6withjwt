a34f12127cbe4d9b0e7ac2bf14e827906339e3ba
Daniel Galvão de Souza <gestordesistemas@gmail.com>
2022-12-22 17:43:38 -0300

    Identity tables and configure with database.
    Implement migration with Identity tables.

---
+++ apiIdentityJwt/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using apiIdentityJwt.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace apiIdentityJwt.Data;
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public ApplicationDbContext()
+    {
+
+    }
+
+    public DbSet<Produto> Produtos { get; set; }
+}
